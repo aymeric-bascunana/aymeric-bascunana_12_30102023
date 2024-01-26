231584f797a0d425249c098f7feb8be0de0618bd
aymeric-bascunana <130646537+aymeric-bascunana@users.noreply.github.com>
2024-01-26 18:22:41 +0100

    modification du 26/01/2024

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import "./ResponsiveHeader.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      {/* <img className="img-header" src={logo}></img> */}
+      <nav>
+        <NavLink className="lien" to="/">
+          Accueil
+        </NavLink>
+        <NavLink className="lien" to="/About">
+          A Propos
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ my-app/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+// import App from "./App";
+
+import reportWebVitals from "./reportWebVitals";
+import Header from "./components/Header/Header";
+// import Footer from "./components/footer/footer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <Home />
+  //<App />
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/Logement/:id" element={<Logement />} /> */}
+        <Route path="/About" element={<About />} />
+        {/* <Route path="*" element={<Error />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
