c686846aeb445b758e19e13b28bb6e63b6bd7fa0
aymeric-bascunana <130646537+aymeric-bascunana@users.noreply.github.com>
2024-02-12 17:52:34 +0100

    modification du 12/02/2024

--- my-app/src/views/Competence/Competence.js
+++ my-app/src/views/Competence/Competence.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "../Competence/Competence.scss";
 import html from "../../assets/logo-html.png";
-// import css from "../../assets/logo-css.png";
-// import sass from "../../assets/logo-sass.png";
-// import javas from "../../assets/logo-js.png";
-// import react from "../../assets/logo-react.png";
+import css from "../../assets/logo-css.png";
+import sass from "../../assets/logo-sass.png";
+import javas from "../../assets/logo-js.png";
+import react from "../../assets/logo-react.png";
 
 function Compétence() {
   return (
     <>
       <section id="Ancre-competence" className="Competence-Contenant">
-        <h2 id="Body-text"> Mes Compétences</h2>
-        <img src={html}></img>
-        {/* <img src={css}></img>
-        <img src={sass}></img>
-        <img src={javas}></img>
-        <img src={react}></img> */}
-        <ul id="Body-text" className="block-list">
+        <h2 id="Body-competence"> Mes Compétences</h2>
+        <ul id="Body-competence" className="block-list">
           <li className="list-Competence">HTML</li>
           <li className="list-Competence">CSS</li>
           <li className="list-Competence">SASS</li>
           <li className="list-Competence">Javascript</li>
           <li className="list-Competence">React</li>
         </ul>
+        <img className="html-img" src={html}></img>
+        <img className="css-img" src={css}></img>
+        <img className="sass-img" src={sass}></img>
+        <img className="js-img" src={javas}></img>
+        <img className="react-img" src={react}></img>
       </section>
     </>
   );
